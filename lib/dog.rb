e5cd0f971b83c690227501c5ed8cae29c7e45cb0
Rebecca Sissman <rsissman15@aol.com>
2022-05-18 11:54:42 -0400

    Completed Assignment

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Dog
-
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed=breed
+    end
+    def breed
+        @breed
+    end
 end
+
+
+d1=Dog.new
+d1.name="rufus"
+puts d1.name
+
